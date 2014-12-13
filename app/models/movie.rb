32ede42e323c846bbf96c2ad3660eeb6b938db9d
Andrews Lartey <enigmageek@outlook.com>
2014-12-13 15:15:02 +0000

    Developed up to including Fig test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,13 +5,13 @@ class Movie < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01 }
   validates :stock, numericality: {only_integer: true }
 
-  # if Rails.env.development?
-  # 	 has_attached_file :poster, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpg"
-  # else
-  # 	has_attached_file :poster, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpg",
-  # 							               :storage => :dropbox,
-  #   						               :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  # end
+  if Rails.env.development?
+  	 has_attached_file :poster, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpg"
+  else
+  	has_attached_file :poster, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpg",
+  							               :storage => :dropbox,
+    						               :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  end
   
-  # validates_attachment_content_type :poster, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :poster, :content_type => /\Aimage\/.*\Z/
 end
